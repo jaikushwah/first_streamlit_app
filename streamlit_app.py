11e8145122541cbcf1c6991490943af872a75c38
Jai Singh Kushwah <114475613+jaikushwah@users.noreply.github.com>
2022-09-29 18:22:13 +0530

    Update streamlit_app.py
    fetching all the rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,11 +49,13 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row=my_cur.fetchone()
+#my_data_row=my_cur.fetchone()#will fetch only one row
+#fetching all the rows
+my_data_rows=my_cur.fetchall()
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
